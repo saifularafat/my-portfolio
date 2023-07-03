95f489db4348dae8a624264bfcf27416522e34c6
saifularafat <saifularafat.info@gmail.com>
2023-07-04 02:08:53 +0600

    ontact section edit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -45,14 +45,12 @@ const Contact = () => {
                             >
 
                             </textarea>
-                            <button>
-                                <a href=''
-                                    className="border-2 group border-sky-500 text-xl tracking-wide  font-medium rounded-md py-2 px-8 relative md:inline-flex flex items-center justify-start overflow-hidden transition-all group">
-                                    <span className="w-0 h-0 rounded bg-sky-500 absolute top-0 left-0 ease-out duration-500 transition-all group-hover:w-full group-hover:h-full -z-1"></span>
-                                    <span className="w-full flex items-center text-sky-400 transition-colors duration-300  ease-in-out group-hover:text-white z-10">
-                                        Submit
-                                    </span>
-                                </a>
+                            <button
+                                className="border-2 group border-sky-500 text-xl tracking-wide  font-medium rounded-md py-2 px-8 relative md:inline-flex flex items-center justify-start overflow-hidden transition-all group">
+                                <span className="w-0 h-0 rounded bg-sky-500 absolute top-0 left-0 ease-out duration-500 transition-all group-hover:w-full group-hover:h-full -z-1"></span>
+                                <span className="w-full flex items-center text-sky-400 transition-colors duration-300  ease-in-out group-hover:text-white z-10">
+                                    Submit
+                                </span>
                             </button>
                         </form>
 
